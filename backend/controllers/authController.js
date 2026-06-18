484e9ce93531f5ce3621b7bc474de5113d9c28a2
Shadab Alam <shadabalam1419@gmail.com>
2026-06-19 02:14:03 +0530

    Change Admin model import to lowercase

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,5 @@
 // backend/controllers/authController.js
-const Admin = require('../models/Admin');
+const Admin = require('../models/admin');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
